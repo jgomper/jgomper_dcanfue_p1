26d66d98e1de45235310d56153c3398f8f066ef1
jgomper <jgomper@upv.edu.es>
2025-10-11 18:06:10 +0200

    Creación inicial del proyecto Flutter con 'flutter create'

---
+++ android/app/src/main/kotlin/upv/epsa/ipc/jgomper_dcanfue_p1/MainActivity.kt
@@ -0,0 +1,5 @@
+package upv.epsa.ipc.jgomper_dcanfue_p1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'views/main_view.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+        title: 'P1 IPC',
+        debugShowCheckedModeBanner: false,
+        home: MainView(), // pantalla principal
+      );
+}
